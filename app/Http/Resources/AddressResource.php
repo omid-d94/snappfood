ef640bfec2f3356c2698f4bf72f5d424f0b7c4f3
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-09 02:07:53 +0330

    feat: make resource for address model
    make AddressCollection for addresses collection
    make AddressResource for indivual address

---
+++ app/Http/Resources/AddressResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AddressResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "title" => $this->title,
+            "address" => $this->address,
+            "latitude" => $this->latitude,
+            "longitude" => $this->longitude,
+        ];
+    }
+}
