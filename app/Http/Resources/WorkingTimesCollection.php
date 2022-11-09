3be426509c5a5f958bb8814d9997f36f15407436
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-09 23:41:10 +0330

    feat: make resource for working times of restaurant

---
+++ app/Http/Resources/WorkingTimesCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class WorkingTimesCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return collect($this->collection)->map(function ($workingTime) {
+            return [
+                $workingTime?->day => [
+                    "start" => $workingTime?->start,
+                    "end" => $workingTime?->end
+                ]
+            ];
+        });
+    }
+}
