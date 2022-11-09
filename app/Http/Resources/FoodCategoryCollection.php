46c0755603b3a413602c8afdfd767eb452b196b2
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-09 23:42:47 +0330

    feat: make resources for food and food category

---
+++ app/Http/Resources/FoodCategoryCollection.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\FoodResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class FoodCategoryCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "categories" => collect($this->collection)
+                ->map(function ($food) {
+                    return [
+                        "id" => $food->foodCategory->id,
+                        "title" => $food->foodCategory->title,
+                        "foods" => new FoodResource($food),
+                    ];
+                })
+        ];
+    }
+}
+
+
+
---
+++ app/Http/Resources/FoodResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FoodResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "title" => $this->title,
+            "price" => $this->price,
+            "raw_material" => $this->raw_material,
+            "image" => $this->image_path
+        ];
+    }
+}
