c83d74d97fec5ff191c693f1067307753e88fc86
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-10 23:13:16 +0330

    feat: make new route for discounts

--- routes/Admin/admin.php
+++ routes/Admin/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\Http\Controllers\admin\DiscountController;
 use App\Http\Controllers\admin\FoodCategoryController;
 use App\Http\Controllers\admin\RestaurantCategoryController;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,6 @@ Route::prefix("admin")->middleware(["admin"])->group(function () {
 
     Route::resource('food-categories', FoodCategoryController::class);
     Route::resource('restaurant-categories', RestaurantCategoryController::class);
-
+    Route::resource("discounts", DiscountController::class);
 });
 
