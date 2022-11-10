08ad07e0b0f14bdbf87981bf769fec259b80d2e3
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-10 23:14:57 +0330

    feat: make request forms for storing and updating discount

---
+++ app/Http/Requests/Admin/UpdateDiscountRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateDiscountRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "title" => ["required", "string", "min:1", "max:100"],
+            "expired_at" => ["after:" . now()->addHour(), "before:" . now()->addMonths(6)],
+            "percent" => ["required", "between:0,100", "numeric"],
+        ];
+    }
+}
