aaaae32b7951cd9d61f7292630f152f0885362f9
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-10 23:06:20 +0330

    feat: mass assing, make accessor and mutator
    make fillable property for mass assignment
    and create accessor and mutator for code property.

--- app/Models/Discount.php
+++ app/Models/Discount.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,8 +10,19 @@ class Discount extends Model
 {
     use HasFactory;
 
+    protected $fillable = ["title", "code", "percent", "expired_at", "factor"];
+
     public function foods()
     {
         return $this->hasMany(Food::class, "discount_id");
     }
+
+    public function code(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => strtoupper($value),
+            set: fn($value) => strtoupper($value)
+        );
+    }
+
 }
