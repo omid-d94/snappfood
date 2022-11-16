03fce21a4f777084404f2d68f3d8edcc62da6d0b
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-16 19:32:24 +0330

    fix: change restaurant controller methods
    change type of  filtering restaurant in index method.
    fix bugs in get foods method.

--- app/Http/Controllers/User/RestaurantController.php
+++ app/Http/Controllers/User/RestaurantController.php
@@ -6,9 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\FoodCategoryCollection;
 use App\Http\Resources\RestaurantCollection;
 use App\Http\Resources\RestaurantResource;
+use App\Models\Food;
+use App\Models\FoodCategory;
 use App\Models\Restaurant;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use mysql_xdevapi\Exception;
 
 class RestaurantController extends Controller
 {
@@ -19,15 +24,22 @@ class RestaurantController extends Controller
      */
     public function index(Request $request)
     {
-
         $restaurants = Restaurant::all();
 
         //filter
         if (count($request->query()) > 0) {
-            $restaurants = Restaurant::type($request->query("type"))
-                ->isOpen($request->query("is_open"))
-                ->scoreGreaterThan($request->query("score_gt"))
-                ->get();
+
+            if ($request->query("type")) {
+                $restaurants = Restaurant::type($request->query("type"))->get();
+            }
+
+            if ($request->query("score_gt")) {
+                $restaurants = Restaurant::scoreGreaterThan($request->query("score_gt"))->get();
+            }
+
+            if ($request->query("is_open")) {
+                $restaurants = Restaurant::restaurantIsOpen($request->query("is_open"))->get();
+            }
         }
         return response(new RestaurantCollection($restaurants), 200);
     }
@@ -49,14 +61,22 @@ class RestaurantController extends Controller
     }
 
     /**
-     * Get foods of restaurant
+     * Get foods of a restaurant
      *
-     * @param Restaurant $restaurant
+     * @param int $id
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|Response
      */
-    public function getFoods(Restaurant $restaurant)
+    public function getFoods(int $id)
     {
-        $foods = $restaurant->foods;
-        return response(new FoodCategoryCollection($foods), 200);
+        $restaurant = Restaurant::where("id", $id)->first();
+
+        if (!is_null($restaurant)) {
+            $foodCategories = $restaurant->foodCategories()
+                ->with("foods", function ($query) use ($restaurant) {
+                    return $query->where("restaurant_id", $restaurant->id);
+                })->get();
+            return response(new FoodCategoryCollection($foodCategories), 200);
+        }
+        return response(["message" => "NOT FOUND!"], 404);
     }
 }
