c754e6d66533ec55880328543d96d6aafd5cb307
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-16 19:09:50 +0330

    fix: change some method in food controller
    change pagination from query builder to eloquent model in index method,
    add discount related to create method,
    fix some bugs in pivot table 'food_category_restaurant'
    and increase performance when seller crud on foods in restaurant.

--- app/Http/Controllers/Seller/FoodController.php
+++ app/Http/Controllers/Seller/FoodController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Foods\FoodRequest;
+use App\Models\Discount;
 use App\Models\Food;
 use App\Models\FoodCategory;
 
@@ -23,7 +24,9 @@ class FoodController extends Controller
     public function index()
     {
         $restaurant = auth("seller")->user()->restaurants->first();
-        $foods = DB::table("foods")->where("restaurant_id", $restaurant?->id)->paginate(10);
+        $foods = Food::where("restaurant_id", $restaurant->id)
+            ->paginate($perPage = 10, $columns = ["*"], $pageName = "foods");
+
         return view("foods.index", compact("foods"));
     }
 
@@ -35,7 +38,8 @@ class FoodController extends Controller
     public function create()
     {
         $categories = FoodCategory::all();
-        return view("foods.create", compact("categories"));
+        $discounts = Discount::all();
+        return view("foods.create", compact("categories", "discounts"));
     }
 
     /**
@@ -46,15 +50,17 @@ class FoodController extends Controller
      */
     public function store(FoodRequest $request)
     {
+        $restaurant = auth("seller")->user()->restaurants->firstOrFail();
         $validated = $request->validated();
 
         //save image to public storage and add image_path to food's table
         $validated["image_path"] = Storage::disk('public')->put('images/foods', $request->file("image_path"));
-
-        $validated["restaurant_id"] = auth("seller")->user()->restaurants->firstOrFail()->id;
-        $validated["food_category"] = FoodCategory::where("title", $request->type)->firstOrFail()->id;
+        $validated["restaurant_id"] = $restaurant->id;
         $food = Food::create($validated);
-        auth("seller")->user()->restaurants->firstOrFail()->foodCategories()->attach($validated["food_category"]);
+        $foodCategories = $restaurant->foodCategories;
+        if (!$foodCategories->contains($food->foodCategory))
+            $restaurant->foodCategories()->attach($food->food_category);
+
         return redirect("/seller/foods")
             ->with("success", "Congradulation!☺ {$food->title} Has Been Created Successfully");
 
@@ -69,7 +75,8 @@ class FoodController extends Controller
     public function show(Food $food)
     {
         $category = $food->foodCategory;
-        return view('foods.show', compact("food", "category"));
+        $discount = $food->discount;
+        return view('foods.show', compact("food", "category", "discount"));
     }
 
     /**
@@ -81,9 +88,9 @@ class FoodController extends Controller
     public function edit(Food $food)
     {
         $categories = FoodCategory::all();
-
+        $discounts = Discount::all();
         return view('foods.edit',
-            compact("food", "categories"));
+            compact("food", "categories", "discounts"));
     }
 
     /**
@@ -96,18 +103,21 @@ class FoodController extends Controller
     public function update(FoodRequest $request, Food $food)
     {
         $validated = $request->validated();
-
         $oldPath = $food->image_path;
-
         if ($request->file("image_path") !== null) {
             Storage::disk('public')->delete($oldPath);
             $imagePath = Storage::disk('public')->put('images/foods', $request->file("image_path"));
         }
-
-        $validated["food_category"] = $request->foodCategory;
-        auth("seller")->user()->restaurants->firstOrFail()->foodCategories()->sync($validated["food_category"]);
         $validated['image_path'] = $imagePath ?? $oldPath;
+        $previousCategory = $food->foodCategory->id;
+        if ($validated["food_category"] !== $previousCategory) {
+            $foods = $food->restaurant->foods()->where("food_category", $previousCategory)->get()->toArray();
+            if (count($foods) === 1)
+                $food->restaurant->foodCategories()->detach($previousCategory);
+        }
         $food->update($validated);
+        if (!$food->restaurant->foodCategories->contains($validated["food_category"]))
+            $food->restaurant->foodCategories()->attach($validated["food_category"]);
 
         return redirect("/seller/foods")
             ->with("success", "Congradulation!☺ {$food->title} Has Been Updated Successfully");
@@ -122,8 +132,14 @@ class FoodController extends Controller
     public function destroy(Food $food)
     {
         Storage::disk('public')->delete($food->image_path);
+        $foodCategory = $food->foodCategory->id;
         $food->delete();
+        $foodCategories = collect($food->restaurant->foods)->map(function ($food) use ($foodCategory) {
+            return $foodCategory === $food->foodCategory->id;
+        })->toArray();
 
+        if (!in_array(true, $foodCategories))
+            $food->restaurant->foodCategories()->detach($foodCategory);
         return redirect("/seller/foods")
             ->with("success", "Congradulation!☺ {$food->title} Has Been Deleted Successfully");
     }
