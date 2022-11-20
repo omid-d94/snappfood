ac5168ca5cc7051d741a319cde3d4700c33a269c
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-21 00:08:03 +0330

    feat: add fillable and table properties

--- app/Models/CartFood.php
+++ app/Models/CartFood.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class CartFood extends Model
+class CartFood extends Pivot
 {
     use HasFactory;
+
+    protected $table = "cart_food";
+    protected $fillable = ["count", "food_id", "cart_id"];
 
 }
