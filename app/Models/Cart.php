0e1f6164e53b37974e0a938b037f0db42d1c1ea8
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-21 22:10:34 +0330

    feat: add paymentCart method
    calculating the amount payable

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
@@ -22,20 +23,48 @@ class Cart extends Model
         )->withPivot("count");
     }
 
+    /**
+     * Relationship between order and cart is one to one
+     *
+     * @return BelongsTo
+     */
     public function order()
     {
         return $this->belongsTo(Order::class);
     }
 
-    /* Relationship between user and cart is one to many */
+    /**
+     * Relationship between user and cart is one to many
+     *
+     * @return BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class, "user_id");
     }
 
-    /* Relationship between restaurant and cart is one to many */
+    /**
+     *  Relationship between restaurant and cart is one to many
+     *
+     * @return BelongsTo
+     */
     public function restaurant()
     {
         return $this->belongsTo(Restaurant::class, "restaurant_id");
+    }
+
+    /**
+     * receive total price for payment
+     *
+     * @param Cart $cart
+     * @return int
+     */
+    public static function paymentCart(Cart $cart): int
+    {
+        $total = 0;
+        foreach (CartFood::where("cart_id", $cart->id)->get() as $item) {
+            $total += Food::find($item->food_id)->price * $item->count;
+        }
+        return $total;
     }
 }
