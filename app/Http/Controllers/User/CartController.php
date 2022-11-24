ecaa7f14c459ebad57c30d2b8b36273ee1634b85
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-25 00:13:52 +0330

    fix: optimize cart controller methods
    add delete food method for detaching food from cart

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Seller\OrderController;
 use App\Http\Requests\Carts\CartRequest;
 use App\Http\Requests\Carts\UpdateCartRequest;
 use App\Http\Resources\CartCollection;
 use App\Http\Resources\CartPaymentResource;
 use App\Http\Resources\CartResource;
+use App\Jobs\SuccessPaymentJob;
 use App\Models\Cart;
 use App\Models\CartFood;
 use App\Models\Food;
+use App\Models\User;
+use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 use function auth;
 use function response;
@@ -88,11 +94,9 @@ class CartController extends Controller
         if (!is_null($cart)) {
             $request->validated();
             $request->authorize();
-            $result = CartFood::where("cart_id", $cart->id)->update(
-                [
-                    "food_id" => $request->input("food_id"),
-                    "count" => $request->input("count"),
-                ]);
+            $result = CartFood::where("cart_id", $cart->id)
+                ->where("food_id", $request->input("food_id"))
+                ->update(["count" => $request->input("count")]);
             if ($result >= 1) {
                 return response(["message" => "Cart updated successfully"], Response::HTTP_NO_CONTENT);
             }
@@ -102,7 +106,7 @@ class CartController extends Controller
     }
 
     /**
-     * Deletes a food cart
+     * Deletes the food cart
      *
      * @param int $cart
      * @return Application|ResponseFactory|\Illuminate\Http\Response
@@ -122,7 +126,7 @@ class CartController extends Controller
      * Shows cart information
      *
      * @param int $cart
-     * @return Application|ResponseFactory|\Illuminate\Http\Response|void
+     * @return Application|\Illuminate\Http\Response|ResponseFactory
      */
     public function showCart(int $cart)
     {
@@ -139,16 +143,56 @@ class CartController extends Controller
      *
      * @param int $cart
      * @return Application|ResponseFactory|\Illuminate\Http\Response
+     * @throws Exception
      */
     public function payForCart(int $cart)
     {
         $cart = Cart::find($cart);
         if (!is_null($cart)) {
             Gate::authorize("owner-cart", $cart);
-            return response(CartPaymentResource::make($cart), Response::HTTP_OK);
+
+            $trackingCode = $this->trackingCode();
+
+            $order = OrderController::store($cart, $trackingCode);
+
+            $this->dispatch(new SuccessPaymentJob(User::find(auth()->user()->id), $order));
+            $cart->delete();
+
+            return response(new CartPaymentResource($order), Response::HTTP_OK);
         }
         return response(["message" => "Cart Not Found!"], Response::HTTP_NOT_FOUND);
     }
 
+    /**
+     * Creator's Tracking Code
+     *
+     * @return string
+     */
+    public function trackingCode(): string
+    {
+        return Str::random(10);
+    }
 
+    /**
+     * Delete the food from cart
+     *
+     * @param int $cart_id
+     * @param int $food
+     * @return Application|ResponseFactory|\Illuminate\Http\Response
+     */
+    public function deleteFood(int $cart_id, int $food)
+    {
+        $cart = Cart::find($cart_id);
+        if (!is_null($cart)) {
+            Gate::authorize("owner-cart", $cart);
+            $result = $cart->foods()->detach($food);
+
+            if ($result == 1) {
+                return response("", Response::HTTP_NO_CONTENT);
+            }
+            return response(["message" => "The food not found in your cart"],
+                Response::HTTP_NOT_FOUND);
+        }
+        return response(["message" => "Cart Not Found!"], Response::HTTP_NOT_FOUND);
+    }
 }
