110516b36753e2ab0a4cbbe16afb6501c8f9f4ab
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-25 00:34:56 +0330

    feat: add table and fillable property

--- app/Models/FoodOrder.php
+++ app/Models/FoodOrder.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class FoodOrder extends Model
+class FoodOrder extends Pivot
 {
     use HasFactory;
+
+    protected $table = "food_order";
+    protected $fillable = ["count", "food_id", "order_id"];
 }
