150bbc1c37cfca6b2b6b4a52fd535398cf60f9bb
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-11-29 01:34:59 +0330

    feat: make two routes for getting and deleting comments

--- routes/Admin/admin.php
+++ routes/Admin/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\Http\Controllers\admin\CommentController;
 use App\Http\Controllers\admin\DiscountController;
 use App\Http\Controllers\admin\FoodCategoryController;
 use App\Http\Controllers\admin\RestaurantCategoryController;
@@ -12,5 +13,10 @@ Route::prefix("admin")->middleware(["admin"])->group(function () {
     Route::resource('food-categories', FoodCategoryController::class);
     Route::resource('restaurant-categories', RestaurantCategoryController::class);
     Route::resource("discounts", DiscountController::class);
+    /* Confirms the seller's request to remove comments */
+    Route::get("/comments", [CommentController::class, "index"])
+        ->name("admin.comments.index");
+    Route::delete("/comments/{comment}", [CommentController::class, "deletingConfirm"])
+        ->name("admin.comments.confirm.delete");
 });
 
