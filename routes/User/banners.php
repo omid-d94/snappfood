0ddefcec16fc32bb35ab514c777e854e19c6258a
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-12-04 20:40:40 +0330

    feat: make route to gets banners in client side

--- routes/User/banners.php
+++ routes/User/banners.php
@@ -1,1 +1,12 @@
 <?php
+
+/* Banners */
+
+use App\Http\Controllers\admin\BannerController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get("/banners", [BannerController::class, "getBanners"])
+    ->name("banners.get");
+
+
