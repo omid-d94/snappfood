e766583748f04e25bd645d5c1f84edca9c05453d
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-12-07 13:00:53 +0330

    fix: fix reports when filtered data

--- app/Exports/ReportExport.php
+++ app/Exports/ReportExport.php
@@ -5,19 +5,37 @@ namespace App\Exports;
 use App\Http\Controllers\Seller\OrderController;
 use App\Http\Controllers\Seller\ReportController;
 use App\Models\Order;
+use App\Traits\Seller\FilteringReports;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithCustomStartCell;
 use Maatwebsite\Excel\Concerns\WithProperties;
+use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\HeadingRowImport;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ReportExport implements FromView, WithCustomStartCell, WithProperties
+class ReportExport implements FromView, WithCustomStartCell, WithProperties, ShouldAutoSize, WithStyles
 {
+    use FilteringReports;
+
+    private Collection $data;
+    private string $from;
+    private string $to;
+
+    public function __construct(Collection $data = null, string $from = null, string $to = null)
+    {
+        $this->data = $data;
+        $this->from = $from;
+        $this->to = $to;
+    }
+
     /**
-     * @return \Illuminate\Support\Collection
+     * @return Collection
      */
-    public function collection()
+    public function collection(): Collection
     {
         return (new ReportController())->getAllOrders();
     }
@@ -33,8 +51,13 @@ class ReportExport implements FromView, WithCustomStartCell, WithProperties
 
     public function view(): View
     {
-        return view("reports.export", ["orders" => $this->collection()]);
-        //(new OrderController())->getArchivedOrders();
+        return view("reports.export",
+            [
+                "orders" => $this->data,
+                "total" => $this->getTotalIncome($this->data)["total"],
+                "from" => $this->from,
+                "to" => $this->to,
+            ]);
     }
 
     public function properties(): array
@@ -49,6 +72,14 @@ class ReportExport implements FromView, WithCustomStartCell, WithProperties
             'category' => 'Invoices',
             'manager' => auth("seller")->user()->name,
             'company' => auth("seller")->user()->restaurants->first()->title,
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+
+        return [
+            1 => ["font" => ["bold" => true], "background" => ["gray"], "color" => ["white"]],
         ];
     }
 }
