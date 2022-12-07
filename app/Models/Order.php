302d2e247d88862c8622da430404b748beb5f146
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-12-07 13:03:18 +0330

    fix: add table name property

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Order extends Model
 {
@@ -78,6 +79,11 @@ class Order extends Model
         return $this->hasOne(Cart::class);
     }
 
+    /**
+     * make accessor for status attribute
+     *
+     * @return Attribute
+     */
     public function status(): Attribute
     {
         return Attribute::make(get: fn($value) => match ($value) {
@@ -89,6 +95,11 @@ class Order extends Model
         });
     }
 
+    /**
+     * make accessor for total attribute
+     *
+     * @return Attribute
+     */
     public function total(): Attribute
     {
         return Attribute::make(get: fn($value) => number_format(
@@ -96,5 +107,118 @@ class Order extends Model
             decimals: 0,
             decimal_separator: "",
             thousands_separator: ','));
+    }
+
+    /**
+     *Get orders last week
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeFilterByWeek($query): mixed
+    {
+        return $query->whereBetween("created_at", [now()->subWeek(), now()]);
+    }
+
+    /**
+     * Get orders last month
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeFilterByMonth($query): mixed
+    {
+        return $query->whereBetween("created_at", [now()->subMonth(), now()]);
+    }
+
+    /**
+     * Get orders when created at between two dates
+     *
+     * @param $query
+     * @param $from
+     * @param $to
+     * @return mixed
+     */
+    public function scopeBetweenTwoDates($query, $from, $to): mixed
+    {
+        return $query->whereBetween("created_at", [$from, $to]);
+    }
+
+    /**
+     * Select total income corresponding to date
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeSelectTotalAndDate($query): mixed
+    {
+        return $query->select(
+            [
+                DB::raw("SUM(total) as total"),
+                DB::raw("Date(created_at) as date")
+            ]);
+    }
+
+    /**
+     * Select count and date of orders
+     * @param $query
+     * @return mixed
+     */
+    public function scopeSelectCountAndDate($query): mixed
+    {
+        return $query->select(
+            [
+                DB::raw("COUNT(*) as count"),
+                DB::raw("Date(created_at) as date")
+            ]);
+    }
+
+    /**
+     * grouping orders by created date
+     * @param $query
+     * @return mixed
+     */
+    public function scopeGroupByCreateAtDate($query): mixed
+    {
+        return $query->groupBy(DB::raw("Date(created_at)"));
+    }
+
+    /**
+     * Select count orders corresponding to month name
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeSelectCountAndMonthName($query): mixed
+    {
+        return $query->select(
+            [
+                DB::raw("COUNT(*) as count"),
+                DB::raw("MONTHNAME(created_at) as month_name")
+            ]);
+    }
+
+    /**
+     * Select income orders corresponding to month name
+     * @param $query
+     * @return mixed
+     */
+    public function scopeSelectTotalAndMonthName($query): mixed
+    {
+        return $query->select(
+            [
+                DB::raw("SUM(total) as income"),
+                DB::raw("MONTHNAME(created_at) as month_name")
+            ]);
+    }
+
+    /**
+     * group orders by month name of created date
+     * @param $query
+     * @return mixed
+     */
+    public function scopeGroupByMonthName($query): mixed
+    {
+        return $query->groupBy(DB::raw("MONTHNAME(created_at)"));
     }
 }
