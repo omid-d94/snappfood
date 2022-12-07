5ae6fd8106afded9a687fd279d69951fd8406672
Omid Daryaei <omiddaryaei.od@gmail.com>
2022-12-08 01:02:39 +0330

    feat: show discount in response of api

--- app/Http/Resources/FoodResource.php
+++ app/Http/Resources/FoodResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class FoodResource extends JsonResource
@@ -9,8 +11,8 @@ class FoodResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param Request $request
+     * @return array|Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
@@ -18,6 +20,7 @@ class FoodResource extends JsonResource
             "id" => $this->id,
             "title" => $this->title,
             "price" => $this->price,
+            "off" => DiscountResource::make($this->resource->discount),
             "raw_material" => $this->raw_material,
             "image" => $this->image_path
         ];
